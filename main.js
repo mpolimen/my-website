80d7e82da089d308eb3b8f99543954c08d739497
mpolimen <stats3434@gmail.com>
2021-09-05 12:21:43 -0400

    Gradient w/ Hedron

--- main.js
+++ main.js
@@ -2,103 +2,66 @@ import './style.css'
 
 import * as THREE from 'three';
 
-// Some Code Credits: https://codepen.io/Mombasa/pen/leiGu
-var $container = $('#container');
-var renderer = new THREE.WebGLRenderer({antialias: true});
-var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-var scene = new THREE.Scene();
-var mouseX = 0, mouseY = 0;
+// https://codepen.io/cubeghost/pen/pJyQRx
+function init() {
+	// var blue = new THREE.Color(0x7658ef);
+	// var pink = new THREE.Color(0xfca4c5);
 
-scene.add(camera);
-renderer.setSize(window.innerWidth, window.innerHeight);
-$container.append(renderer.domElement);
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+	var renderer = new THREE.WebGLRenderer({ alpha: true });
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
-window.addEventListener( 'resize', onWindowResize, false );
-/////////////////////////////////////////
+	var shape = [];
+	var geometry = new THREE.IcosahedronGeometry(2.5,0);
+	var material = new THREE.MeshNormalMaterial();
+	shape[0] = new THREE.Mesh( geometry, material );
+	shape[1] = new THREE.Mesh( geometry, material );
+	shape[2] = new THREE.Mesh( geometry, material );
+	shape[0].position.set(0,5,0);
+	shape[1].position.set(0,5,0);
+	shape[2].position.set(0,5,0);
+	scene.add(shape[0],shape[1],shape[2]);
 
-// Console
-var Controls = function() {
-  this.speed = 1;
-  this.rotation = 0;
-};
+	var light = new THREE.PointLight(0xfca4c5);
+	light.position.set(0,250,0);
+	scene.add(light);
 
-var text = new Controls()
-//     gui = new dat.GUI();
-//     gui.add(text, 'speed', 0, 10);
-//     gui.add(text, 'rotation',0,15);
+	camera.position.set(-3,4,10); // x y z
 
-/////////////////////////////////////////
+	function render() {
+		requestAnimationFrame( render );
 
-// Normalmaterial
-var normalMaterial = new THREE.MeshNormalMaterial({});
-
-
-// Torus
-function Torus(f){
-  this.b = new THREE.Mesh(new THREE.TorusGeometry( 160, 75, 2, 13),normalMaterial);
-  this.b.position.x = 57*Math.cos(f);
-  this.b.position.y = 57*Math.sin(f);
-  this.b.position.z = f*1.25;
-  this.b.rotation.z = f*0.03;
-}
-		
-var numTorus = 80;
-var tabTorus = [];
-for(var i=0; i<numTorus; i++){
-  tabTorus.push(new Torus(-i*13));
-  scene.add(tabTorus[i].b);
-}	
-
-// Gallery
-const texture_me = new THREE.TextureLoader().load('images/me.jpg');
-const me = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: texture_me }));
-scene.add(me);
-me.position.z = -7;
-me.position.x = 3;
-
-function spin() {
-  const t = document.body.getBoundingClientRect().top;
-
-  me.rotation.y += 0.01;
-  me.rotation.z += 0.01;
-  me.position.y = t * -.02;
-
-}
-document.body.onscroll = spin;
-
-// Update
-function update(){
-  for(var i=0; i<numTorus; i++){
-    tabTorus[i].b.position.z+=text.speed;
-    tabTorus[i].b.rotation.z+=i*text.rotation/10000;
-    if(tabTorus[i].b.position.z>0)
-    {
-      tabTorus[i].b.position.z=-1000;
-    }
-  }
+		shape[0].rotation.x += 0.035;
+		shape[0].rotation.y -= 0.005;
+		shape[1].rotation.y += 0.015;
+		shape[1].rotation.z -= 0.005;
+		shape[2].rotation.z -= 0.025;
+		shape[2].rotation.x += 0.005;
+		renderer.render(scene, camera);
+	}
+	render();
+	
 }
 
-function onWindowResize() {
-    // windowHalfX = window.innerWidth / 2;
-    // windowHalfY = window.innerHeight / 2;
+init();
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+// // Gallery
+// const texture_me = new THREE.TextureLoader().load('images/me.jpg');
+// const me = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: texture_me }));
+// scene.add(me);
+// me.position.z = -7;
+// me.position.x = 3;
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
+// function spin() {
+//   const t = document.body.getBoundingClientRect().top;
 
-// Render
-function render() {
-  requestAnimationFrame(render);
+//   me.rotation.y += 0.01;
+//   me.rotation.z += 0.01;
+//   me.position.y = t * -.02;
 
-  camera.position.x += (mouseX - camera.position.x ) * .05;
-  camera.position.y += (-mouseY - camera.position.y ) * .05;
-
-  renderer.render(scene, camera);
-  update();
-}
-
-render();
-spin();
+// }
+// document.body.onscroll = spin;
+// spin();
